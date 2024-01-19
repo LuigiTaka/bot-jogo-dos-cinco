8a6fc260989b1d1f328d5de7a180f2714f1f0dbb
Luigi <luigimendeszanchett@gmail.com>
2024-01-19 11:08:30 -0300

    refactor: Remove comando

--- commands.js
+++ commands.js
@@ -1,42 +1,10 @@
 import 'dotenv/config';
-import {getRPSChoices} from './game.js';
 import {capitalize, InstallGlobalCommands, getPerguntas} from './utils.js';
-
-// Get the game choices from game.js
-function createCommandChoices() {
-    const choices = getRPSChoices();
-    const commandChoices = [];
-
-    for (let choice of choices) {
-        commandChoices.push({
-            name: capitalize(choice),
-            value: choice.toLowerCase(),
-        });
-    }
-
-    return commandChoices;
-}
 
 // Simple test command
 const TEST_COMMAND = {
     name: 'test',
     description: 'Basic command',
-    type: 1,
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-    name: 'challenge',
-    description: 'Challenge to a match of rock paper scissors',
-    options: [
-        {
-            type: 3,
-            name: 'object',
-            description: 'Pick your object',
-            required: true,
-            choices: createCommandChoices(),
-        },
-    ],
     type: 1,
 };
 
@@ -87,6 +55,6 @@ const ADICIONA_RESPOSTA_COMMAND = {
         }
     ]
 }
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, PERGUNTA_COMMAND, RESPOSTA_COMMAND, FIM_COMMAND, ADICIONA_RESPOSTA_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, PERGUNTA_COMMAND, RESPOSTA_COMMAND, FIM_COMMAND, ADICIONA_RESPOSTA_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
